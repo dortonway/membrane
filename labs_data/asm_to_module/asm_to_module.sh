252daabdd7b578bf8360ea592884a816ae6a2ff8
dortonway <dortonway@gmail.com>
2015-01-05 23:56:02 +0300

    added x8051 compiler, linker and loader

---
+++ labs_data/asm_to_module/asm_to_module.sh
@@ -0,0 +1,3 @@
+cp $1 labs_data/asm_to_module/PR.ASM
+dosbox labs_data/asm_to_module/combine.bat
+labs_data/asm_to_module/loader labs_data/asm_to_module/PR.TSK
---
+++ labs_data/asm_to_module/combine.bat
@@ -0,0 +1,5 @@
+mount c /home/i/Dropbox/development/js/atomic_membrane_1.2/membrane/labs_data/asm_to_module
+C:
+X8051.EXE -Q PR.ASM -ld
+LINK51.EXE -Q PR.LNK
+exit
